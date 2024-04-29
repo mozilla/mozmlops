b20d45550ba1b163559d98e780a32bf3e6b9f451
Chelsea Troy <chelsea.dommert@gmail.com>
2024-04-29 16:39:39 -0500

    advance version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Artifact store tools'
 LONG_DESCRIPTION = 'Tools for interacting with GCS in our model flows'
 
