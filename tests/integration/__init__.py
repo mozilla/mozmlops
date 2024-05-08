f87b23064b330f3aa0c3bbd327d874fc665e847e
Chelsea Troy <chelsea.dommert@gmail.com>
2024-05-08 10:18:08 -0500

    Add link to ticket for fixing TODO

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -2,4 +2,5 @@ import sys
 import os
 
 # To avoid ModuleNotFound errors in tests while attempting to import test subjects.
+# To be investigated here: https://mozilla-hub.atlassian.net/browse/DENG-3667
 sys.path.append(f"{os.getcwd()}/src")
